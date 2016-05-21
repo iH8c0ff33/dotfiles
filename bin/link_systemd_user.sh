8c6cfdb27f59f5324ab9138a5dd11a43fa2715d9
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-05-21 12:04:50 +0200

    fixed error
    ln was using $(basename $i) in first argument which resulted in a non-working
    symlink

--- bin/link_systemd_user.sh
+++ bin/link_systemd_user.sh
@@ -2,5 +2,5 @@
 [ -e ~/.config/systemd/user ] || mkdir -p ~/.config/systemd/user
 for i in systemd_user/*
 do
-  [ -e ~/.config/systemd/user/$(basename $i) ] && echo $i already exists || ln -s $PWD/$(basename $i) ~/.config/systemd/user/
+  [ -e ~/.config/systemd/user/$(basename $i) ] && echo $i already exists || ln -s $PWD/$i ~/.config/systemd/user/
 done
