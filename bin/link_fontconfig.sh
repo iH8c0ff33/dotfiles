ea43147d07d512c1f5e2375d2bd8b71b35ea7de6
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-05-22 14:48:55 +0200

    fixed typo

--- bin/link_fontconfig.sh
+++ bin/link_fontconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo ---SYMLINKING FONTCONFIG---
 [ -e ~/.config/fontconfig ] || mkdir -p ~/.config/fontconfig ]
-[ -e ~/.config/fontconfig/fonts.conf ] && echo fonts.conf already exists || ln -s $PWD/font-config ~/.config/fontconfig/
+[ -e ~/.config/fontconfig/fonts.conf ] && echo fonts.conf already exists || ln -s $PWD/font-config ~/.config/fontconfig/fonts.conf
